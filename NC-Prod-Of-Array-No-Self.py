d9b37d6125dee12ca4df9b56abd3d634d4b0877a
Ella <ellaberman9@gmail.com>
2024-06-22 23:36:27 -0500

    Add files via upload

---
+++ NC-Prod-Of-Array-No-Self.py
@@ -0,0 +1,20 @@
+# Given an integer array nums, return an array output where output[i] is the product of all the elements of nums except nums[i].
+
+# Each product is guaranteed to fit in a 32-bit integer.
+
+# Follow-up: Could you solve it in 𝑂(𝑛) time without using the 
+# division operation?
+
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        # O(n) time, O(1) space
+        res = [1] * (len(nums))
+
+        for i in range(1, len(nums)):
+            res[i] = res[i-1] * nums[i-1]
+
+        postfix = 1
+        for i in range(len(nums) - 1, -1, -1):
+            res[i] *= postfix
+            postfix *= nums[i]
+        return res
