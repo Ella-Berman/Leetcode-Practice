b52e30f3f6cd41ed4ca1f5a76a292f15e0abc466
Ella <ellaberman9@gmail.com>
2024-07-08 16:59:33 -0700

    Add files via upload

---
+++ NC-Longest-Palindromic-SS.py
@@ -0,0 +1,32 @@
+# Given a string s, return the longest substring of s that is a palindrome.
+
+# A palindrome is a string that reads the same forward and backward.
+
+# If there are multiple palindromic substrings that have the same length, return any one of them.
+
+class Solution:
+    # check if each char is the CENTER of a palindrome
+    def longestPalindrome(self, s: str) -> str:
+        res = ""
+        resLen = 0
+
+        for i in range(len(s)):
+            # odd length
+            l, r = i, i
+            while l >= 0 and r < len(s) and s[l] == s[r]:
+                if (r - l + 1) > resLen:
+                    res = s[l : r + 1]
+                    resLen = r - l + 1
+                l -= 1
+                r += 1
+
+            # even length
+            l, r = i, i + 1
+            while l >= 0 and r < len(s) and s[l] == s[r]:
+                if (r - l + 1) > resLen:
+                    res = s[l : r + 1]
+                    resLen = r - l + 1
+                l -= 1
+                r += 1
+
+        return res
